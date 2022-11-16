28e2018f77e9fc7a39cf13b53b8b346d1bba8b20
caien <caien811_P@proton.me>
2022-11-16 12:57:08 +0545

    added wallpaper

--- dots/qtile-shakura/qtile/autostart.sh
+++ dots/qtile-shakura/qtile/autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-feh --bg-scale /home/caien/.config/qtile/Wallpapers/sci-fi.jpg
+feh --bg-scale /home/caien/.config/qtile/Wallpapers/shakura-panda.png
 picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
 
 # Low battery notifier
